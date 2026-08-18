ce1b6fc7153b87748f1afffa017936630c00b7d1
harsha r <harsharharshar19@gmail.com>
2026-08-19 00:54:20 +0530

    Fix dashboard import path

--- taskmanagement-frontend/taskmanager-frontend/src/App.jsx
+++ taskmanagement-frontend/taskmanager-frontend/src/App.jsx
@@ -8,7 +8,7 @@ import {
 
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
-import Dashboard from "./Pages/Dashboard/DashBoard";
+import Dashboard from "./Pages/Dashboard/Dashboard";
 
 import ProtectedRoute
     from "./Components/ProtectedRoute/ProtectedRoute";
