58a37dcb783e27375122416d58c1b8ff3ee143b7
harsha r <harsharharshar19@gmail.com>
2026-08-19 08:40:40 +0530

    Remove frontend debug logs

--- taskmanagement-frontend/taskmanager-frontend/src/services/api.js
+++ taskmanagement-frontend/taskmanager-frontend/src/services/api.js
@@ -8,8 +8,6 @@ api.interceptors.request.use((config) => {
 
     const token = localStorage.getItem("token");
 
-    console.log("API REQUEST:", config.url);
-    console.log("TOKEN EXISTS:", !!token);
 
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
